be249fe394be3743d225eae427ca1a451d3564f8
Sara <sarah.alalaya@gmail.com>
2016-07-20 13:49:40 +0300

    Challenge18 parcially completed

---
+++ spec/features/enter_names_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+
+RSpec.feature "Adding names" do
+  scenario "User enters a new name" do
+    visit "/"
+
+    fill_in :player_1_name, :with => "Sarah"
+    fill_in :player_2_name, :with => "Prashant"
+    click_button "Submit"
+    expect(page).to have_text("Sarah vs. Prashant")
+  end
+end
