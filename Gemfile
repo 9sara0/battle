bf2e3765bc1c072102415e6d1c1a2415d1e1a632
prashantmathias <mathiasaudio@gmail.com>
2016-07-20 14:05:06 +0100

    on ex18

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ ruby "2.3.1"
 gem 'sinatra'
 gem 'rspec-sinatra'
 gem "capybara"
+gem 'rspec'
--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -8,6 +8,7 @@ RSpec.feature "Adding names" do
     fill_in :player_1_name, :with => "Sarah"
     fill_in :player_2_name, :with => "Prashant"
     click_button "Submit"
+
     expect(page).to have_text("Sarah vs. Prashant")
   end
 end
